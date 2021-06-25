ae5ffeb2ff01d5a4e0593e05516f89449b3150e6
Maxwell Gordon <maxwelljgordon@gmail.com>
2021-06-24 18:40:26 -0600

    Increase Typescript support

--- src/index.d.ts
+++ src/index.d.ts
@@ -4,13 +4,22 @@ import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer
 import { ConfigOptions, GlobeInstance as GlobeKapsuleInstance } from 'globe.gl';
 
 type Accessor<In, Out> = Out | string | ((obj: In) => Out);
-type ObjAccessor<T> = Accessor<object, T>;
+type ObjAccessor<T, In extends object = object> = Accessor<In, T>;
 type HexBinAccessor<T> = Accessor<HexBin, T>;
 
+type ArcDataType = GlobeProps['arcsData'][number];
+type CustomLayerDataType = GlobeProps['customLayerData'][number];
+type HexBinPointDataType = GlobeProps['hexBinPointsData'][number];
+type HexPolygonDataType = GlobeProps['hexPolygonsData'][number];
+type LabelDataType = GlobeProps['labelsData'][number];
+type PointDataType = GlobeProps['pointsData'][number];
+type PolygonDataType = GlobeProps['polygonsData'][number];
+type TileDataType = GlobeProps['tilesData'][number];
+
 interface HexBin {
-  points: object[],
-  sumWeight: number,
-  center: { lat: number, lng: number }
+  points: PointDataType[];
+  sumWeight: number;
+  center: { lat: number; lng: number };
 }
 
 interface GeoJsonGeometry {
@@ -56,61 +65,61 @@ export interface GlobeProps extends ConfigOptions {
   atmosphereAltitude?: number;
   globeMaterial?: Material;
   onGlobeReady?: () => void;
-  onGlobeClick?: (coords: { lat, lng }, event: MouseEvent) => void;
-  onGlobeRightClick?: (coords: { lat, lng }, event: MouseEvent) => void;
+  onGlobeClick?: (coords: { lat; lng }, event: MouseEvent) => void;
+  onGlobeRightClick?: (coords: { lat; lng }, event: MouseEvent) => void;
 
   // Points layer
   pointsData?: object[];
-  pointLat?: ObjAccessor<number>;
-  pointLng?: ObjAccessor<number>;
-  pointColor?: ObjAccessor<string>;
-  pointAltitude?: ObjAccessor<number>;
-  pointRadius?: ObjAccessor<number>;
+  pointLat?: ObjAccessor<number, PointDataType>;
+  pointLng?: ObjAccessor<number, PointDataType>;
+  pointColor?: ObjAccessor<string, PointDataType>;
+  pointAltitude?: ObjAccessor<number, PointDataType>;
+  pointRadius?: ObjAccessor<number, PointDataType>;
   pointResolution?: number;
   pointsMerge?: boolean;
   pointsTransitionDuration?: number;
-  pointLabel?: ObjAccessor<string>;
-  onPointClick?: (point: object, event: MouseEvent) => void;
-  onPointRightClick?: (point: object, event: MouseEvent) => void;
-  onPointHover?: (point: object | null, prevPoint: object | null) => void;
+  pointLabel?: ObjAccessor<string, PointDataType>;
+  onPointClick?: (point: PointDataType, event: MouseEvent) => void;
+  onPointRightClick?: (point: PointDataType, event: MouseEvent) => void;
+  onPointHover?: (point: PointDataType | null, prevPoint: PointDataType | null) => void;
 
   // Arcs layer
   arcsData?: object[];
-  arcStartLat?: ObjAccessor<number>;
-  arcStartLng?: ObjAccessor<number>;
-  arcEndLat?: ObjAccessor<number>;
-  arcEndLng?: ObjAccessor<number>;
-  arcColor?: ObjAccessor<string | string[]>;
-  arcAltitude?: ObjAccessor<number | null>;
-  arcAltitudeAutoScale?: ObjAccessor<number>;
-  arcStroke?: ObjAccessor<number | null>;
+  arcStartLat?: ObjAccessor<number, ArcDataType>;
+  arcStartLng?: ObjAccessor<number, ArcDataType>;
+  arcEndLat?: ObjAccessor<number, ArcDataType>;
+  arcEndLng?: ObjAccessor<number, ArcDataType>;
+  arcColor?: ObjAccessor<string | string[], ArcDataType>;
+  arcAltitude?: ObjAccessor<number | null, ArcDataType>;
+  arcAltitudeAutoScale?: ObjAccessor<number, ArcDataType>;
+  arcStroke?: ObjAccessor<number | null, ArcDataType>;
   arcCurveResolution?: number;
   arcCircularResolution?: number;
-  arcDashLength?: ObjAccessor<number>;
-  arcDashGap?: ObjAccessor<number>;
-  arcDashInitialGap?: ObjAccessor<number>;
-  arcDashAnimateTime?: ObjAccessor<number>;
+  arcDashLength?: ObjAccessor<number, ArcDataType>;
+  arcDashGap?: ObjAccessor<number, ArcDataType>;
+  arcDashInitialGap?: ObjAccessor<number, ArcDataType>;
+  arcDashAnimateTime?: ObjAccessor<number, ArcDataType>;
   arcsTransitionDuration?: number;
-  arcLabel?: ObjAccessor<string>;
-  onArcClick?: (arc: object, event: MouseEvent) => void;
-  onArcRightClick?: (arc: object, event: MouseEvent) => void;
-  onArcHover?: (arc: object | null, prevArc: object | null) => void;
+  arcLabel?: ObjAccessor<string, ArcDataType>;
+  onArcClick?: (arc: ArcDataType, event: MouseEvent) => void;
+  onArcRightClick?: (arc: ArcDataType, event: MouseEvent) => void;
+  onArcHover?: (arc: ArcDataType | null, prevArc: ArcDataType | null) => void;
 
   // Polygons layer
   polygonsData?: object[];
-  polygonGeoJsonGeometry?: ObjAccessor<GeoJsonGeometry>;
-  polygonCapColor?: ObjAccessor<string>;
-  polygonCapMaterial?: ObjAccessor<Material>;
-  polygonSideColor?: ObjAccessor<string>;
-  polygonSideMaterial?: ObjAccessor<Material>;
-  polygonStrokeColor?: ObjAccessor<string | boolean | null>;
-  polygonAltitude?: ObjAccessor<number>;
-  polygonCapCurvatureResolution?: ObjAccessor<number>;
+  polygonGeoJsonGeometry?: ObjAccessor<GeoJsonGeometry, PolygonDataType>;
+  polygonCapColor?: ObjAccessor<string, PolygonDataType>;
+  polygonCapMaterial?: ObjAccessor<Material, PolygonDataType>;
+  polygonSideColor?: ObjAccessor<string, PolygonDataType>;
+  polygonSideMaterial?: ObjAccessor<Material, PolygonDataType>;
+  polygonStrokeColor?: ObjAccessor<string | boolean | null, PolygonDataType>;
+  polygonAltitude?: ObjAccessor<number, PolygonDataType>;
+  polygonCapCurvatureResolution?: ObjAccessor<number, PolygonDataType>;
   polygonsTransitionDuration?: number;
-  polygonLabel?: ObjAccessor<string>;
-  onPolygonClick?: (polygon: object, event: MouseEvent) => void;
-  onPolygonRightClick?: (polygon: object, event: MouseEvent) => void;
-  onPolygonHover?: (polygon: object | null, prevPolygon: object | null) => void;
+  polygonLabel?: ObjAccessor<string, PolygonDataType>;
+  onPolygonClick?: (polygon: PolygonDataType, event: MouseEvent) => void;
+  onPolygonRightClick?: (polygon: PolygonDataType, event: MouseEvent) => void;
+  onPolygonHover?: (polygon: PolygonDataType | null, prevPolygon: PolygonDataType | null) => void;
 
   // Paths layer
   pathsData?: object[];
@@ -132,10 +141,10 @@ export interface GlobeProps extends ConfigOptions {
   onPathHover?: (path: object | null, prevPath: object | null) => void;
 
   // Hex Bin layer
-  hexBinPointsData?: object[];
-  hexBinPointLat?: ObjAccessor<number>;
-  hexBinPointLng?: ObjAccessor<number>;
-  hexBinPointWeight?: ObjAccessor<number>;
+  hexBinPointsData?: HexBin[];
+  hexBinPointLat?: ObjAccessor<number, HexBinPointDataType>;
+  hexBinPointLng?: ObjAccessor<number, HexBinPointDataType>;
+  hexBinPointWeight?: ObjAccessor<number, HexBinPointDataType>;
   hexBinResolution?: number;
   hexMargin?: HexBinAccessor<number>;
   hexAltitude?: HexBinAccessor<number>;
@@ -151,62 +160,73 @@ export interface GlobeProps extends ConfigOptions {
 
   // Hexed Polygons layer
   hexPolygonsData?: object[];
-  hexPolygonGeoJsonGeometry?: ObjAccessor<GeoJsonGeometry>;
-  hexPolygonColor?: ObjAccessor<string>;
-  hexPolygonAltitude?: ObjAccessor<number>;
-  hexPolygonResolution?: ObjAccessor<number>;
-  hexPolygonMargin?: ObjAccessor<number>;
-  hexPolygonCurvatureResolution?: ObjAccessor<number>;
+  hexPolygonGeoJsonGeometry?: ObjAccessor<GeoJsonGeometry, HexPolygonDataType>;
+  hexPolygonColor?: ObjAccessor<string, HexPolygonDataType>;
+  hexPolygonAltitude?: ObjAccessor<number, HexPolygonDataType>;
+  hexPolygonResolution?: ObjAccessor<number, HexPolygonDataType>;
+  hexPolygonMargin?: ObjAccessor<number, HexPolygonDataType>;
+  hexPolygonCurvatureResolution?: ObjAccessor<number, HexPolygonDataType>;
   hexPolygonsTransitionDuration?: number;
-  hexPolygonLabel?: ObjAccessor<string>;
-  onHexPolygonClick?: (polygon: object, event: MouseEvent) => void;
-  onHexPolygonRightClick?: (polygon: object, event: MouseEvent) => void;
-  onHexPolygonHover?: (polygon: object | null, prevPolygon: object | null) => void;
+  hexPolygonLabel?: ObjAccessor<string, HexPolygonDataType>;
+  onHexPolygonClick?: (polygon: HexPolygonDataType, event: MouseEvent) => void;
+  onHexPolygonRightClick?: (polygon: HexPolygonDataType, event: MouseEvent) => void;
+  onHexPolygonHover?: (
+    polygon: HexPolygonDataType | null,
+    prevPolygon: HexPolygonDataType | null
+  ) => void;
 
   // Tiles layer
   tilesData?: object[];
-  tileLat?: ObjAccessor<number>;
-  tileLng?: ObjAccessor<number>;
-  tileAltitude?: ObjAccessor<number>;
-  tileWidth?: ObjAccessor<number>;
-  tileHeight?: ObjAccessor<number>;
-  tileUseGlobeProjection?: ObjAccessor<boolean>;
-  tileMaterial?: ObjAccessor<Material>;
-  tileCurvatureResolution?: ObjAccessor<number>;
+  tileLat?: ObjAccessor<number, TileDataType>;
+  tileLng?: ObjAccessor<number, TileDataType>;
+  tileAltitude?: ObjAccessor<number, TileDataType>;
+  tileWidth?: ObjAccessor<number, TileDataType>;
+  tileHeight?: ObjAccessor<number, TileDataType>;
+  tileUseGlobeProjection?: ObjAccessor<boolean, TileDataType>;
+  tileMaterial?: ObjAccessor<Material, TileDataType>;
+  tileCurvatureResolution?: ObjAccessor<number, TileDataType>;
   tilesTransitionDuration?: number;
-  tileLabel?: ObjAccessor<string>;
-  onTileClick?: (tile: object, event: MouseEvent) => void;
-  onTileRightClick?: (tile: object, event: MouseEvent) => void;
-  onTileHover?: (tile: object | null, prevTile: object | null) => void;
+  tileLabel?: ObjAccessor<string, TileDataType>;
+  onTileClick?: (tile: TileDataType, event: MouseEvent) => void;
+  onTileRightClick?: (tile: TileDataType, event: MouseEvent) => void;
+  onTileHover?: (tile: TileDataType | null, prevTile: TileDataType | null) => void;
 
   // Labels layer
   labelsData?: object[];
-  labelLat?: ObjAccessor<number>;
-  labelLng?: ObjAccessor<number>;
-  labelText?: ObjAccessor<string>;
-  labelColor?: ObjAccessor<string>;
-  labelAltitude?: ObjAccessor<number>;
-  labelSize?: ObjAccessor<number>;
+  labelLat?: ObjAccessor<number, LabelDataType>;
+  labelLng?: ObjAccessor<number, LabelDataType>;
+  labelText?: ObjAccessor<string, LabelDataType>;
+  labelColor?: ObjAccessor<string, LabelDataType>;
+  labelAltitude?: ObjAccessor<number, LabelDataType>;
+  labelSize?: ObjAccessor<number, LabelDataType>;
   labelTypeFace?: TypeFace;
-  labelRotation?: ObjAccessor<number>;
+  labelRotation?: ObjAccessor<number, LabelDataType>;
   labelResolution?: number;
-  labelIncludeDot?: ObjAccessor<boolean>;
-  labelDotRadius?: ObjAccessor<number>;
-  labelDotOrientation?: ObjAccessor<LabelOrientation>;
+  labelIncludeDot?: ObjAccessor<boolean, LabelDataType>;
+  labelDotRadius?: ObjAccessor<number, LabelDataType>;
+  labelDotOrientation?: ObjAccessor<LabelOrientation, LabelDataType>;
   labelsTransitionDuration?: number;
-  labelLabel?: ObjAccessor<string>;
-  onLabelClick?: (label: object, event: MouseEvent) => void;
-  onLabelRightClick?: (label: object, event: MouseEvent) => void;
-  onLabelHover?: (label: object | null, prevLabel: object | null) => void;
+  labelLabel?: ObjAccessor<string, LabelDataType>;
+  onLabelClick?: (label: LabelDataType, event: MouseEvent) => void;
+  onLabelRightClick?: (label: LabelDataType, event: MouseEvent) => void;
+  onLabelHover?: (label: LabelDataType | null, prevLabel: LabelDataType | null) => void;
 
   // Custom layer
   customLayerData?: object[];
-  customThreeObject?: Object3D | string | ((d: object, globeRadius: number) => Object3D);
-  customThreeObjectUpdate?: string | ((obj: Object3D, objData: object, globeRadius: number) => void);
-  customLayerLabel?: ObjAccessor<string>;
-  onCustomLayerClick?: (obj: object, event: MouseEvent) => void;
-  onCustomLayerRightClick?: (obj: object, event: MouseEvent) => void;
-  onCustomLayerHover?: (obj: object | null, prevObj: object | null) => void;
+  customThreeObject?:
+    | Object3D
+    | string
+    | ((d: CustomLayerDataType, globeRadius: number) => Object3D);
+  customThreeObjectUpdate?:
+    | string
+    | ((obj: Object3D, objData: CustomLayerDataType, globeRadius: number) => void);
+  customLayerLabel?: ObjAccessor<string, CustomLayerDataType>;
+  onCustomLayerClick?: (obj: CustomLayerDataType, event: MouseEvent) => void;
+  onCustomLayerRightClick?: (obj: CustomLayerDataType, event: MouseEvent) => void;
+  onCustomLayerHover?: (
+    obj: CustomLayerDataType | null,
+    prevObj: CustomLayerDataType | null
+  ) => void;
 
   // Render control
   enablePointerInteraction?: boolean;
@@ -218,7 +238,10 @@ export interface GlobeProps extends ConfigOptions {
 export interface GlobeMethods {
   // Render control
   pointOfView(): GeoCoords;
-  pointOfView(pov: { lat?: number, lng?: number, altitude?: number }, transitionMs?: number): GlobeKapsuleInstance;
+  pointOfView(
+    pov: { lat?: number; lng?: number; altitude?: number },
+    transitionMs?: number
+  ): GlobeKapsuleInstance;
   pauseAnimation(): GlobeKapsuleInstance;
   resumeAnimation(): GlobeKapsuleInstance;
   scene(): Scene;
@@ -231,10 +254,12 @@ export interface GlobeMethods {
   getCoords(lat: number, lng: number, altitude?: number): CartesianCoords;
   getScreenCoords(lat: number, lng: number, altitude?: number): ScreenCoords;
   toGeoCoords(coords: CartesianCoords): GeoCoords;
-  toGlobeCoords(x: number, y: number): { lat: number, lng: number} | null;
+  toGlobeCoords(x: number, y: number): { lat: number; lng: number } | null;
 }
 
-type FCwithRef<P = {}, R = {}> = React.FunctionComponent<P & { ref?: React.MutableRefObject<R | undefined> }>;
+type FCwithRef<P = {}, R = {}> = React.FunctionComponent<
+  P & { ref?: React.MutableRefObject<R | undefined> }
+>;
 
 declare const Globe: FCwithRef<GlobeProps, GlobeMethods>;
 
